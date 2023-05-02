2e5159546f25ba369747da264d9260ea728ca55c
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-05-02 08:48:24 +0200

    Add the kata for tuesday

---
+++ tuesday.php
@@ -0,0 +1,9 @@
+<?php
+/***
+ * 1/ Lire le fichier message.txt (utiliser file_get_contents() => voir la doc php)
+ * 
+ * 2/ remplacer [PRENOM] par votre prénom
+ * 
+ * 3/   afficher le resultat
+ * 
+ */
